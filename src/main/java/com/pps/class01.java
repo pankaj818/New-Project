fcafa1f526adae3693112493a9b139729647b691
pankaj818 <pankajprakash81@gmail.com>
2019-11-21 17:10:53 +0530

    my new repo

---
+++ src/main/java/com/pps/class01.java
@@ -0,0 +1,8 @@
+package com.pps;
+
+public class class01 {
+	public void NewClass() {
+		System.out.println("class01");
+	}
+
+}
---
+++ src/test/java/com/pps/Test/Class01Test.java
@@ -0,0 +1,8 @@
+package com.pps.Test;
+
+public class Class01Test {
+	public void Class01Test() {
+		System.out.println("class01Test");
+	}
+
+}
